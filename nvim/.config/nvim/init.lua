df6f831347764cadedf9649012493ec584c7d9fd
JSkahl <josekahl333@gmail.com>
2025-03-08 16:49:09 -0300

    solving problems in neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,20 @@
--- Load core settings
+-- Load config settings
 require 'config.options'
 require 'config.keymaps'
-require 'config.autocmds'
 
--- Load plugin manager
-require 'plugins.lazy'
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
 
 -- Setup plugins
 require('lazy').setup 'plugins'
