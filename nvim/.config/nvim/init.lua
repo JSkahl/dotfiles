7df5ce50b5fda41cec716cc8d82c0a91fa0ad6ec
JSkahl <josekahl333@gmail.com>
2025-03-08 16:24:58 -0300

    organizing nvim init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,76 +1,10 @@
----------------------------
---         NeoVim        --
---         config        --
----------------------------
+-- Load core settings
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocmds'
 
-vim.g.mapleader = ' ' -- Set the leader key to space
-vim.g.maplocalleader = ' ' -- Set the local leader key to space
-vim.g.have_nerd_font = true -- Enable support for Nerd Fonts
+-- Load plugin manager
+require 'plugins.lazy'
 
-vim.opt.number = true -- Show line numbers
-vim.opt.mouse = 'a' -- Enable mouse support in all modes
-vim.opt.showmode = false -- Don't show mode in command line (like -- INSERT --)
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
-end)
-vim.opt.breakindent = true -- Enable break indent
-vim.opt.undofile = true -- Save undo history
-vim.opt.ignorecase = true -- Ignore case in search patterns
-vim.opt.smartcase = true -- Override ignorecase if search pattern contains uppercase
-vim.opt.signcolumn = 'yes' -- Always show the sign column (for diagnostics, etc.)
-vim.opt.updatetime = 250 -- Faster completion and diagnostic updates
-vim.opt.timeoutlen = 300 -- Shorter timeout for key mappings
-vim.opt.splitright = true -- Open vertical splits to the right
-vim.opt.splitbelow = true -- Open horizontal splits below
-vim.opt.list = true -- Show invisible characters
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- Define how invisible characters look
-vim.opt.inccommand = 'split' -- Show preview of substitutions in a split
-vim.opt.cursorline = true -- Highlight the current line
-vim.opt.scrolloff = 10 -- Keep 10 lines visible above/below the cursor
-
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- Clear search highlights with Esc
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' }) -- Open diagnostic list
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }) -- Exit terminal mode with Esc Esc
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
+-- Setup plugins
 require('lazy').setup 'plugins'
--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,9 +1,4 @@
 return {
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  'navarasu/onedark.nvim',
+  priority = 1000,
 }
