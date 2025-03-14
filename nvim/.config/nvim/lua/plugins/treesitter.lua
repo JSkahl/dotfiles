3d4a5f1934d58ee2d800d003a66f2156a8eea949
JSkahl <josekahl333@gmail.com>
2025-03-14 19:32:24 -0300

    Updating TS

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,19 +5,19 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "ruby",
-          "html",
-          "css",
-          "scss",
-          "javascript",
-          "typescript",
-          "json",
-          "lua",
-        },
-        highlight = { enable = true },
+        auto_install = true,
+        -- ensure_installed = {
+        --   "bash",
+        --   "ruby",
+        --   "html",
+        --   "css",
+        --   "scss",
+        --   "javascript",
+        --   "typescript",
+        --   "json",
+        --   "lua",
+        -- },
+        -- highlight = { enable = true },
         indent = { enable = false },
       })
     end
