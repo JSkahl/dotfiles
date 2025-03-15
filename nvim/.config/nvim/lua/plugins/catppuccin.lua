0f47ac4b862471111bd5c71335490f108ab68bfb
JSkahl <josekahl333@gmail.com>
2025-03-15 02:13:58 -0300

    Updating theme

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -8,8 +8,17 @@ return {
     config = function()
       require("catppuccin").setup({
         transparent_background = true,
+        custom_highlights = function(colors)
+          return {
+            LineNr = { fg = colors.overlay1 },                    -- Change line number color
+            CursorLineNr = { fg = colors.lavender, bold = true }, -- Highlighted line number
+            VertSplit = { fg = colors.overlay0 },                 -- Split border color
+            WinSeparator = { fg = colors.overlay0 },              -- Window separator (useful for Explorer)
+            SpecialKey = { fg = colors.lavender },                -- Paths and comments
+          }
+        end,
       })
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
 }
